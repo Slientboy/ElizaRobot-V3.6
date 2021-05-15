cfd40be2c24f5a70a233be233f353868e89cfd52
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-15 18:53:59 +0500

    Update __init__.py

--- ElizaRobot/__init__.py
+++ ElizaRobot/__init__.py
@@ -5,6 +5,7 @@ import time
 import spamwatch
 
 import telegram.ext as tg
+from redis import StrictRedis
 from pyrogram import Client, errors
 from telethon import TelegramClient
 
@@ -69,6 +70,7 @@ if ENV:
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
+    REDIS_URL = os.environ.get('REDIS_URL')
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
@@ -140,6 +142,7 @@ else:
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
+    REDIS_URL = Config.REDIS_URL
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
     TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
@@ -171,6 +174,25 @@ DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 DEV_USERS.add(1200780834)
 DEV_USERS.add(797768146) 
+
+REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
+
+try:
+
+    REDIS.ping()
+
+    LOGGER.info("Your redis server is now alive!")
+
+except BaseException:
+
+    raise Exception("Your redis server is not alive, please check again.")
+
+finally:
+
+   REDIS.ping()
+
+   LOGGER.info("Your redis server is now alive!")
+
 
 if not SPAMWATCH_API:
     sw = None
