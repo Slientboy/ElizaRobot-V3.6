a7d4ccd3947df12d8a5fcf13ac8a720ef5f49a81
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-18 23:51:37 +0500

    Update __init__.py

--- ElizaRobot/__init__.py
+++ ElizaRobot/__init__.py
@@ -208,9 +208,9 @@ telethn = TelegramClient("layla", API_ID, API_HASH)
 pbot = Client("elizapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 pgram = Client("eliza", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
-mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
-db = motor[MONGO_DB]
+mongodb = MongoClient(MONGO_DB_URL)
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
+db = motor[MONGO_DB_URL]
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
